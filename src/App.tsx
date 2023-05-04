0f470ee30483332fed58ce01b0a2fb5559f6a1ab
Muhammad Umer <omerawan445@gmail.com>
2023-05-05 00:20:02 +0500

    Setup Project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import Watch from "./pages/Watch";
 
 const App = () => {
  return (
-  <div className="flex flex-col space-y-4 bg-stone-700">
-  App
-</div>
+  <BrowserRouter>
+  <Routes>
+  <Route path="/search" element={<Search />}/>
+  <Route path="/watch/:id" element={<Watch />}/>
+  <Route path="/" element={<Home/>}/>
+  </Routes>
+  </BrowserRouter>
  )
 }
 
