88093d9df56530ca1dbd7b5a2486232f7b7c7799
Muhammad Umer <omerawan445@gmail.com>
2023-05-21 03:00:00 +0500

    Completed the HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import Search from "./pages/Search";
 import Watch from "./pages/Watch";
 
 const App = () => {
+/*  Apply a feature that when a  user enters in the home page load the dummy data as youtube does
+    and when data is fetched (Hint: promise is resolved) then replace the dummmy data with the data
+ */
  return (
   <BrowserRouter>
   <Routes>
